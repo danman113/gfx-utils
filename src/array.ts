43f0148b423b2807391e8711b4e44383691350ba
Daniel <danman113@msn.com>
2022-02-17 21:11:42 -0800

    Add ringbuffer

--- src/array.ts
+++ src/array.ts
@@ -9,3 +9,36 @@ export const shuffle = <T>(array: T[]) => {
   }
   return array
 }
+
+export class RingBuffer<T> {
+  data: T[]
+  start: number = 0
+  end: number = 0
+  length: number = 0
+  constructor(public size: number, public fill: T = null) {
+    this.data = Array(size).fill(null)
+  }
+
+  push(elem: T): boolean {
+    const overwrite = (this.length + 1) > this.size
+    this.length = Math.min(this.length + 1, this.size)
+    let oldEnd = this.end 
+    this.data[this.end++] = elem
+    this.end = this.end % this.size
+    // Move the front pointer so we only overwrite the oldest entry
+    if (oldEnd == this.start && this.length > 1) this.start = ((this.start + 1) % this.size)
+    return overwrite
+  }
+
+  popFront(): T {
+    this.length = Math.max(0, this.length - 1)
+    const data = this.data[this.start]
+    this.data[this.start++] = this.fill
+    this.start = this.start % this.size
+    return data
+  }
+
+  peekFront(): T {
+    return this.data[this.start]
+  }
+}
